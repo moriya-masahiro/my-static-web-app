24704458a3ebcc745969449e47f7c74a7669473b
Masahiro Moriya <moriya-masahiro@fujitsu.com>
2021-12-15 15:22:29 +0900

    First commit.

--- angular-app/src/app/products/products.component.ts
+++ angular-app/src/app/products/products.component.ts
@@ -16,15 +16,19 @@ import { ProductService } from './product.service';
           <app-product-list [products]="products"></app-product-list>
         </div>
       </div>
-      <div class="new field">
+    </div>
+
+    <div class="demonstration 1">
+      <app-list-header
+        title="デモ1"
+        (refresh)="getProducts()"
+      ></app-list-header>
+      <div class="columns is-multiline is-variable">
         <div class="column is-8" *ngIf="products$ | async as products">
           <app-product-list [products]="products"></app-product-list>
         </div>
       </div>
-      <div class="hoge">
-        <p>hogehoge</p>
-      </div>
-      <div>
+    </div>
   `,
 })
 export class ProductsComponent implements OnInit {
