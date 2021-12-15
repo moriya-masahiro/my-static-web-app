46cc2ff5adfbb66f1241fabf9edd2b2c417fbb81
Masahiro Moriya <moriya-masahiro@fujitsu.com>
2021-12-15 12:53:45 +0900

    First commit.

--- api/HttpTrigger1/__init__.py
+++ api/HttpTrigger1/__init__.py
@@ -1,35 +1,20 @@
 import logging
-
+import io
+import numpy as np
 import azure.functions as func
-from PIL import Image
+import matplotlib
+import matplotlib.pyplot as plt
 
+matplotlib.use('Agg')
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+  logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-    
-
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        red = Image.new('RGBA', (255, 255), (255, 0, 0, 0))
-        response = func.HttpResponse(content_type="image/jpeg")
-        red.save(response, "JPEG")
-        return response
-
-        """
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
-        """
+  image = io.BytesIO()
+  x = np.linspace(0, 10)
+  y = np.sin(x)
+  plt.plot(x, y)
+  plt.savefig(image, format='png')
+  image.seek(0)
+  return func.HttpResponse(image.read(),mimetype='image/png')
         
